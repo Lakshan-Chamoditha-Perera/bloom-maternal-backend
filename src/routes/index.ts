8a0d211ef37d5289ff933ce68c19ce5949ba19fa
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-08-29 16:43:43 +0530

    Adds mother and medical records routes
    Adds routes for mothers and medical records to the main router.
    
    This modification enables access to the new features related to mother and medical record management.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import authRoutes from './auth.routes';
 import clinicRoutes from './clinic.routes';
+import motherRouter from './mother.routes';
+import medicleRecordsRouter from './medicleRecord.routes';
+
 import { authenticateToken } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/clinics', clinicRoutes);
+router.use("/mothers", motherRouter)
+router.use("/medicle-records", medicleRecordsRouter)
 
 
 export default router; 
